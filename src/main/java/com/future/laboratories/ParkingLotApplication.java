c54ccd9fcb6def86f633f9489e3e1a7b59f0778f
bockgareth <bockgareth@gmail.com>
2018-11-13 12:16:01 +0200

    Java documentation added for main application class.

--- src/main/java/com/future/laboratories/ParkingLotApplication.java
+++ src/main/java/com/future/laboratories/ParkingLotApplication.java
@@ -6,8 +6,15 @@ import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * This class acts as the application starting point.
+ */
 public class ParkingLotApplication extends Application {
 
+    /**
+     * Set used to store app-wide singleton providers. These
+     * providers live throughout the lifetime of the application.
+     */
     private Set<Object> singletons = new HashSet<>();
 
     public ParkingLotApplication() {
