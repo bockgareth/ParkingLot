581cf6013068ef5732fb28b5c5a37480ddcd6116
bockgareth <bockgareth@gmail.com>
2018-11-13 09:02:18 +0200

    Support for the mode of payments due during the day of a month, added to service layer.

--- src/main/java/com/future/laboratories/service/ReportService.java
+++ src/main/java/com/future/laboratories/service/ReportService.java
@@ -6,4 +6,5 @@ public interface ReportService {
     public ReportDto getReportByMonth(int month);
     public ReportDto getReportByDay(int month, int day);
     public ReportDto getLostTicketCount(int month, int day);
+    public ReportDto getAmountDueMode(int month, int day);
 }
--- src/main/java/com/future/laboratories/service/impl/ReportServiceImpl.java
+++ src/main/java/com/future/laboratories/service/impl/ReportServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ReportServiceImpl implements ReportService {
@@ -72,12 +73,54 @@ public class ReportServiceImpl implements ReportService {
         return returnValue;
     }
 
+    public ReportDto getAmountDueMode(int month, int day) {
+        ReportDto returnValue = new ReportDto();
+
+        List<TicketEntity> tickets = ticketDao.getAllTickets();
+
+        List<Integer> values = new ArrayList<>();
+
+        for (TicketEntity ticket: tickets) {
+            if (ticket.getTicketDate().getMonth().getValue() == month && ticket.getTicketDate().getDayOfMonth() == day) {
+                values.add(ticket.getAmountDue());
+            }
+        }
+
+        returnValue.setMode(mode(values));
+
+        return returnValue;
+    }
+
     public int reduce(List<Integer> values) {
         int sum = 0;
         for (int value: values) {
             sum += value;
         }
         return sum;
+    }
+
+    public int mode(List<Integer> values) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        int max  = 1;
+        int temp = 0;
+
+        for(int value: values) {
+            if (hashMap.get(value) != null) {
+                int count = hashMap.get(value);
+
+                count++;
+                hashMap.put(value, count);
+
+                if(count > max) {
+                    max  = count;
+                    temp = value;
+                }
+            } else {
+                hashMap.put(value, 1);
+            }
+        }
+        return temp;
     }
 
 }
