56c4963df8b41c00acf86177ab6ed8e32bea0717
bockgareth <bockgareth@gmail.com>
2018-11-14 09:11:04 +0200

    Added response model for creating a ticket.

---
+++ src/main/java/com/future/laboratories/ui/model/response/ticket/CreatedTicketResponseModel.java
@@ -0,0 +1,47 @@
+package com.future.laboratories.ui.model.response.ticket;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class CreatedTicketResponseModel {
+
+    private int ticketId;
+    private LocalDate ticketDate;
+    private LocalTime enterTime;
+
+    public CreatedTicketResponseModel() {
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public LocalDate getTicketDate() {
+        return ticketDate;
+    }
+
+    public void setTicketDate(LocalDate ticketDate) {
+        this.ticketDate = ticketDate;
+    }
+
+    public LocalTime getEnterTime() {
+        return enterTime;
+    }
+
+    public void setEnterTime(LocalTime enterTime) {
+        this.enterTime = enterTime;
+    }
+
+    @Override
+    public String toString() {
+        return "CreatedTicketResponseModel{" +
+                "ticketId=" + ticketId +
+                ", ticketDate=" + ticketDate +
+                ", enterTime=" + enterTime +
+                '}';
+    }
+}
