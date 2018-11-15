bf1f9d4997814b36c80ad2f44d1fed5f1b735f9e
bockgareth <bockgareth@gmail.com>
2018-11-15 11:38:31 +0200

    Added new response model for reports generated by the week of the year.

---
+++ src/main/java/com/future/laboratories/ui/model/response/report/RevenueWeekResponseModel.java
@@ -0,0 +1,39 @@
+package com.future.laboratories.ui.model.response.report;
+
+/**
+ * This class acts as a response model for
+ * when the client requests a report on the revenue
+ * generated for the week of the year.
+ */
+public class RevenueWeekResponseModel {
+
+    private int week;
+    private int revenue;
+
+    public RevenueWeekResponseModel() {
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public int getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(int revenue) {
+        this.revenue = revenue;
+    }
+
+    @Override
+    public String toString() {
+        return "RevenueWeekResponseModel{" +
+                "week=" + week +
+                ", revenue=" + revenue +
+                '}';
+    }
+}
