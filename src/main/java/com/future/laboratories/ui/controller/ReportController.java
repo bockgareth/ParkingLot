20c78dc05c781fec6376b8acc876b3fcf62207e9
bockgareth <bockgareth@gmail.com>
2018-11-15 11:41:16 +0200

    Report controller now supports generating a report based on the week of the year.

--- src/main/java/com/future/laboratories/ui/controller/ReportController.java
+++ src/main/java/com/future/laboratories/ui/controller/ReportController.java
@@ -3,10 +3,7 @@ package com.future.laboratories.ui.controller;
 import com.future.laboratories.service.ReportService;
 import com.future.laboratories.service.impl.ReportServiceImpl;
 import com.future.laboratories.shared.dto.ReportDto;
-import com.future.laboratories.ui.model.response.report.LostTicketCountResponseModel;
-import com.future.laboratories.ui.model.response.report.PaymentModeResponseModel;
-import com.future.laboratories.ui.model.response.report.RevenueMonthDayResponseModel;
-import com.future.laboratories.ui.model.response.report.RevenueMonthResponseModel;
+import com.future.laboratories.ui.model.response.report.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -15,7 +12,7 @@ import javax.ws.rs.core.MediaType;
  * This class acts as the report controller that hosts HTTP endpoints
  * for report related queries.
  */
-@Path("/")
+@Path("/reports")
 public class ReportController {
 
     /**
@@ -27,10 +24,10 @@ public class ReportController {
      * GET (on 200 OK) request to retrieve a report summary for the specified
      * month.
      * @param month the month for the report to be generated on.
-     * @return a ReportDto as a response, output declared as json.
+     * @return a RevenueMonthResponseModel as a response, output declared as json.
      */
     @GET
-    @Path("/reports/{month}")
+    @Path("/{month}")
     @Produces(MediaType.APPLICATION_JSON)
     public RevenueMonthResponseModel getReportByMonth(@PathParam("month") int month) {
         RevenueMonthResponseModel returnValue = new RevenueMonthResponseModel();
@@ -48,10 +45,10 @@ public class ReportController {
      * month and day.
      * @param month the month for the report to be generated on.
      * @param day the day for the report to be generated on.
-     * @return a ReportDto as a response, output declared as json.
+     * @return a RevenueMonthDayResponseModel as a response, output declared as json.
      */
     @GET
-    @Path("/reports/{month}/{day}")
+    @Path("/{month}/{day}")
     @Produces(MediaType.APPLICATION_JSON)
     public RevenueMonthDayResponseModel getReportByDay(@PathParam("month") int month, @PathParam("day") int day) {
         RevenueMonthDayResponseModel returnValue = new RevenueMonthDayResponseModel();
@@ -66,14 +63,33 @@ public class ReportController {
     }
 
     /**
+     * GET (on 200 OK) request to retrieve a report summary for the specified week.
+     * @param week the week for the report to be generated on.
+     * @return a RevenueWeekResponseModel as a response, output declared as json.
+     */
+    @GET
+    @Path("/week/{week}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public RevenueWeekResponseModel getReportByWeek(@PathParam("week") int week) {
+        RevenueWeekResponseModel returnValue = new RevenueWeekResponseModel();
+
+        ReportDto reportDto = reportService.getReportByWeek(week);
+
+        returnValue.setWeek(week);
+        returnValue.setRevenue(reportDto.getRevenue());
+
+        return returnValue;
+    }
+
+    /**
      * GET (on 200 OK) request to retrieve a report summary on how many
      * tickets were lost for the specified month and day.
      * @param month the month for the report to be generated on.
      * @param day the day for the report to be generated on.
-     * @return a ReportDto as a response, output declared as json.
+     * @return a LostTicketCountResponseModel as a response, output declared as json.
      */
     @GET
-    @Path("/reports/{month}/{day}/lost")
+    @Path("/{month}/{day}/lost")
     @Produces(MediaType.APPLICATION_JSON)
     public LostTicketCountResponseModel getLostTicketCount(@PathParam("month") int month, @PathParam("day") int day) {
         LostTicketCountResponseModel returnValue = new LostTicketCountResponseModel();
@@ -92,11 +108,11 @@ public class ReportController {
      * of payments due for the specified month and day.
      * @param month the month for the report to be generated on.
      * @param day the day for the report to be generated on.
-     * @return a ReportDto as a response, output declared as json. Only supports
+     * @return a PaymentModeResponseModel as a response, output declared as json. Only supports
      * one mode. If there are mode conflicts, response will result in a 0.
      */
     @GET
-    @Path("/reports/{month}/{day}/mode")
+    @Path("/{month}/{day}/mode")
     @Produces(MediaType.APPLICATION_JSON)
     public PaymentModeResponseModel getAmountDueMode(@PathParam("month") int month, @PathParam("day") int day) {
         PaymentModeResponseModel returnValue = new PaymentModeResponseModel();
