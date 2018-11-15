0d765933416f90a151d92b5a23061e35e804ea31
Gareth Bock <bockgareth@gmail.com>
2018-11-15 19:09:43 +0200

    Added test cases for report service.

---
+++ src/test/java/com/future/laboratories/service/impl/ReportServiceImplTest.java
@@ -0,0 +1,83 @@
+package com.future.laboratories.service.impl;
+
+import com.future.laboratories.entity.TicketEntity;
+import com.future.laboratories.shared.dao.impl.TicketDaoImpl;
+import com.future.laboratories.shared.dto.ReportDto;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class ReportServiceImplTest {
+
+    @InjectMocks
+    ReportServiceImpl reportService;
+
+    @Mock
+    TicketDaoImpl ticketDao;
+
+    TicketEntity ticketEntity;
+
+    final int TICKET_ID = 1;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        ticketEntity = new TicketEntity();
+        ticketEntity.setTicketId(TICKET_ID);
+        ticketEntity.setTicketDate(LocalDate.of(2018, 11, 12));
+        ticketEntity.setEnterTime(LocalTime.of(10, 30));
+        ticketEntity.setExitTime(LocalTime.of(11, 30));
+        ticketEntity.setTicketLost(false);
+        ticketEntity.setAmountDue(20);
+    }
+
+    @Test
+    public void getReportByMonth() {
+        when(ticketDao.getAllTickets()).thenReturn(Arrays.asList(ticketEntity));
+
+        ReportDto reportDto = reportService.getReportByMonth(11);
+
+        assertNotNull(reportDto);
+        assertEquals(20, reportDto.getRevenue());
+    }
+
+    @Test
+    public void getReportByDay() {
+        when(ticketDao.getAllTickets()).thenReturn(Arrays.asList(ticketEntity));
+
+        ReportDto reportDto = reportService.getReportByDay(11, 12);
+
+        assertNotNull(reportDto);
+        assertEquals(20, reportDto.getRevenue());
+    }
+
+    @Test
+    public void getLostTicketCount() {
+        when(ticketDao.getAllTickets()).thenReturn(Arrays.asList(ticketEntity));
+
+        ReportDto reportDto = reportService.getLostTicketCount(11, 12);
+
+        assertNotNull(reportDto);
+        assertEquals(0, reportDto.getLost());
+    }
+
+    @Test
+    public void getAmountDueMode() {
+        when(ticketDao.getAllTickets()).thenReturn(Arrays.asList(ticketEntity));
+
+        ReportDto reportDto = reportService.getAmountDueMode(11, 12);
+
+        assertNotNull(reportDto);
+        assertEquals(0, reportDto.getMode());
+    }
+}
