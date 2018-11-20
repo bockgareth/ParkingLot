c3fb7ab793c47c34202d014fb5f2fe78c7cd9db2
bockgareth <bockgareth@gmail.com>
2018-11-20 12:27:51 +0200

    Added new ticket request model.

---
+++ src/main/java/com/future/laboratories/ticket/api/model/UpdatedTicketRequestModel.java
@@ -0,0 +1,50 @@
+package com.future.laboratories.ticket.api.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * This class acts as a request model for closing a ticket.
+ */
+public class UpdatedTicketRequestModel {
+
+    private LocalDate ticketDate;
+    private LocalTime exitTime;
+    private boolean ticketLost;
+
+    public UpdatedTicketRequestModel() {
+    }
+
+    public LocalDate getTicketDate() {
+        return ticketDate;
+    }
+
+    public void setTicketDate(LocalDate ticketDate) {
+        this.ticketDate = ticketDate;
+    }
+
+    public LocalTime getExitTime() {
+        return exitTime;
+    }
+
+    public void setExitTime(LocalTime exitTime) {
+        this.exitTime = exitTime;
+    }
+
+    public boolean isTicketLost() {
+        return ticketLost;
+    }
+
+    public void setTicketLost(boolean ticketLost) {
+        this.ticketLost = ticketLost;
+    }
+
+    @Override
+    public String toString() {
+        return "UpdatedTicketRequestModel{" +
+                "ticketDate=" + ticketDate +
+                ", exitTime=" + exitTime +
+                ", ticketLost=" + ticketLost +
+                '}';
+    }
+}
