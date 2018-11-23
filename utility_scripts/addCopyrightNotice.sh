773a12763de21c3c58837e47ac03d5de75a45700
bockgareth <bockgareth@gmail.com>
2018-11-23 14:22:48 +0200

    Added script to add copyright notices to java classes.

---
+++ utility_scripts/addCopyrightNotice.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This script will add a copyright notice to any java files that do not have a copyright notice.
+COPYRIGHT_NOTICE="COPYRIGHT © $(date +"%G") GARETH BOCK AND/OR ITS AFFILIATES. ALL RIGHTS RESERVED."
+JAVA_COPYRIGHT_NOTICE="/* ${COPYRIGHT_NOTICE} */"
+
+# Store all java class without copy right notice in an array
+FILES_WITHOUT_COPYRIGHT=($(find ../src -type f -name "*.java" -print0 | xargs -0 grep --files-without-match -E 'COPYRIGHT © [[:digit:]]* GARETH BOCK [[:alpha:]]*'))
+
+#
+for i in ${!FILES_WITHOUT_COPYRIGHT[@]};
+do
+    echo ${FILES_WITHOUT_COPYRIGHT[i]}
+    sed -i "1s;^;$JAVA_COPYRIGHT_NOTICE;\n" ${FILES_WITHOUT_COPYRIGHT[i]}
+done
